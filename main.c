cbaabb0ab47024838067a314f1e1209a5a808334
maximzakharov <zakharov.m89@gmail.com>
2017-02-10 12:23:25 +0400

    rm macros

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
-#include <CppUTest/UtestMacros.h>
+// #include <CppUTest/UtestMacros.h>
 #include <stdio.h>
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/CommandLineTestRunner.h"
